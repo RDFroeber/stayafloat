68c09ff27fee977edb94712769c3c1b5244b6792
RaleighD <RDFroeber@gmail.com>
2014-02-04 17:31:34 -0500

    Index, new, create, show action methods added to user controller

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -17,12 +17,94 @@
 
 module.exports = {
 
+	index: function(req, res) {
+		User.findAll(function(err, users){
+			if (err) return res.send(err, 500);
+
+			res.view({
+				model: users
+			});
+		});
+  },
+
 	new: function(req, res) {
 		res.view();
   },
- 	create: function(req, res) {
 
+ 	create: function(req, res, next) {
+ 		var userObj = {
+      username: req.param('username'),
+      password: req.param('password'),
+      confirmation: req.param('confirmation')
+    }
+
+    // Create a User with the params sent from 
+    User.create(userObj, function userCreated(err, user) {
+
+      // If there's an error
+      if(err) {
+        console.log(err);
+
+        // If error redirect back to sign-up page
+        // return res.redirect('/user/new');
+      }
+      res.json(user);
+
+    //   // Log user in
+    //   // req.session.authenticated = true;
+    //   // req.session.User = user;
+
+    //   user.save(function(err, user) {
+    //     if(err) {
+    //     	console.log(err);
+  				
+  		// 		return next(err);
+  		// 	}
+
+    //   // Let other subscribed sockets know that the user was created.
+    //   User.publishCreate(user);
+
+    //     // After successfully creating the user redirect to the show action
+    //     res.redirect('/user/show/' + user.id);
+    //   });
+    });
+  },
+
+  show: function(req, res, next) {
+    User.findOne(req.param('id'), function foundUser(err, user) {
+      if (err) return next(err);
+      if (!user) return next();
+      res.view({
+        user: user
+      });
+    });
   }
+
+  // login: function(req, res) {
+  //   var bcrypt = require('bcrypt');
+
+  //   User.findOneByEmail(req.body.email).done(function (err, user) {
+  //     if (err) res.json({ error: 'DB error' }, 500);
+
+  //     if (user) {
+  //       bcrypt.compare(req.body.password, user.password, function (err, match) {
+  //         if (err) res.json({ error: 'Server error' }, 500);
+
+  //         if (match) {
+  //           // password match
+  //           req.session.user = user.id;
+  //           res.json(user);
+  //         } else {
+  //           // invalid password
+  //           if (req.session.user) req.session.user = null;
+  //           res.json({ error: 'Invalid password' }, 400);
+  //         }
+  //       });
+  //     } else {
+  //       res.json({ error: 'User not found' }, 404);
+  //     }
+  //   });
+  // }
 
 
   /**
