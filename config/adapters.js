e9df1a00628861f6b2b38cabba7706c7e93d8dc6
RaleighD <RDFroeber@gmail.com>
2014-02-04 11:58:08 -0500

    PostgreSQL added and adapter configured

--- config/adapters.js
+++ config/adapters.js
@@ -24,16 +24,13 @@ module.exports.adapters = {
     module: 'sails-disk'
   },
 
-  // MySQL is the world's most popular relational database.
-  // Learn more: http://en.wikipedia.org/wiki/MySQL
-  myLocalMySQLDatabase: {
-
-    module: 'sails-mysql',
-    host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user: 'YOUR_MYSQL_USER',
-    // Psst.. You can put your password in config/local.js instead
-    // so you don't inadvertently push it up if you're using version control
-    password: 'YOUR_MYSQL_PASSWORD', 
-    database: 'YOUR_MYSQL_DB'
+  // PostgreSQL
+  config: {
+    database: 'aFloat_db',
+    host: 'localhost',
+    user: 'freelancer',
+    password: 'Infor2014',
+    port: 5432,
+    pool: false
   }
 };
