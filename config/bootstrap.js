7df5d5bfbe1b38f6268054dcb89e9b7ccc21703e
RaleighD <RDFroeber@gmail.com>
2014-02-07 17:20:20 -0500

    Admin user creatoin added to bootstrap file

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -9,7 +9,23 @@
  */
 
 module.exports.bootstrap = function (cb) {
-
+	var adminObj = {
+		username: 'admin', 
+		password: 'theBoss', 
+		confirmation: 'theBoss', 
+		admin: true
+  }
+  var admin = User.findOneByUsername('admin')
+	// If no admin is found...
+	if(!admin) {
+		// Create an admin
+	  User.create(adminObj, function adminCreated(err, newAdmin) {
+	    newAdmin.save(function(err, newAdmin) {
+	      // On creation success redirect to admin main
+	      // return res.redirect('/user');
+	    });
+	  });
+	}
   // It's very important to trigger this callack method when you are finished 
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
   cb();
