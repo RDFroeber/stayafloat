fe8a85d7dd753d644ad34691d5ef87ff4fe6c33e
RaleighD <RDFroeber@gmail.com>
2014-02-07 17:22:11 -0500

    Navigation updated in layout view and policies updated for pages

--- config/policies.js
+++ config/policies.js
@@ -16,7 +16,17 @@ module.exports.policies = {
 
   // Default policy for all controllers and actions
   // (`true` allows public access) 
-  '*': true
+  '*': ['flash','isAuthenticated'],
+
+  SessionController: {
+  	'new': true
+  },
+
+  // UserController: {
+  // 	'*': 'isAdmin',
+  // 	index: true,
+  // 	destroy:  true
+  // }
 
   /*
 	// Here's an example of adding some policies to a controller
